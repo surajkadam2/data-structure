7d1ae07d8f9fd835c82096b93d142504839fbc9c
Suraj Kadam <surajkadam2@gmail.com>
2020-01-25 21:37:44 +0530

    C1-D1
    Array

---
+++ src/com/datastructure/array/oned/ArrayCreation.java
@@ -0,0 +1,78 @@
+package com.datastructure.array.oned;
+
+import com.datastructure.utilts.FormatUtils;
+
+public class ArrayCreation {
+
+	public static void main(String[] args) {
+
+		// Create int array
+		int[] intA = { 1, 2, 3, 4, 5 };
+
+		// print Array
+		for (int i = 0; i < intA.length; i++) {
+			System.out.println(i);
+		}
+		FormatUtils.printLine();
+
+		// Create int array with size
+		int[] B = new int[10];
+
+		// add values
+		for (int i = 0; i < B.length; i++) {
+			B[i] = i;
+		}
+
+		// print Array
+		for (int i = 0; i < B.length; i++) {
+			System.out.println(B[i]);
+		}
+		FormatUtils.printLine();
+
+		// Retrieve element from array from position
+		int index = 25;
+		if (index < B.length) {
+			System.out.println(B[index]);
+			FormatUtils.printLine();
+		}
+
+		int keyToFind = 9;
+		boolean isPresent = false;
+		// Is array contains a key
+		for (int i = 0; i < B.length; i++) {
+			if (B[i] == keyToFind) {
+				isPresent = true;
+				break;
+			}
+		}
+		System.out.println(isPresent);
+		FormatUtils.printLine();
+
+		// delete int 5 from B array
+		int key = 5;
+
+		// Find the index of i
+		int indexOfKey = 0;
+		for (int i = 0; i < B.length; i++) {
+			if (B[i] == key) {
+				indexOfKey = i;
+			}
+		}
+
+		// remove element
+		// Determine new size after removal
+		int newLen = B.length - 1;
+
+		// start from index of key upto new len
+		// Assign next element to current
+		for (int i = indexOfKey; i < newLen; i++) {
+			B[i] = B[i + 1];
+		}
+
+		for (int i = 0; i < B.length; i++) {
+			System.out.println(B[i]);
+		}
+		FormatUtils.printLine();
+
+	}
+}
---
+++ src/com/datastructure/utilts/FormatUtils.java
@@ -0,0 +1,9 @@
+package com.datastructure.utilts;
+
+public class FormatUtils {
+	
+	public static void printLine() {
+		System.out.println("-------------------------------------");
+	}
+
+}
