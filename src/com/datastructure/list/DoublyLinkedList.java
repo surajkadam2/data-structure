b2adc23db9a0c6e1354515621f7a36760f6d9aa0
Suraj Kadam <surajkadam2@gmail.com>
2020-01-27 21:38:43 +0530

    C5

--- src/com/datastructure/list/DoublyLinkedList.java
+++ src/com/datastructure/list/DoublyLinkedList.java
@@ -1,7 +1,5 @@
 package com.datastructure.list;
 
-import org.apache.commons.lang3.StringUtils;
-
 import com.datastructure.utilts.ConsoleLogger;
 
 /**
@@ -10,7 +8,7 @@ import com.datastructure.utilts.ConsoleLogger;
  */
 public class DoublyLinkedList {
 
-	static Node head = null;
+	static Node headNode = null;
 
 	static DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
 
@@ -34,10 +32,120 @@ public class DoublyLinkedList {
 		printWithAddresses();
 		ConsoleLogger.printLine();
 
+		// get nth node from list
+		ConsoleLogger.printLine();
+		getNodeByIndex(1);
+		ConsoleLogger.printLine();
+		getNodeByIndex(3);
+		ConsoleLogger.printLine();
+
+		// get node that contains data
+		int k = 100;
+		getNodeByData(66);
+
+		ConsoleLogger.printLine();
+
+		// delete nth node
+		deleteNthNode(1);
+		printDLL();
+		ConsoleLogger.printLine();
+
+		deleteNthNode(5);
+		printDLL();
+		ConsoleLogger.printLine();
+
+		// Delete node that contais data
+		deleteNodeThatContainsData(3);
+		printDLL();
+		ConsoleLogger.printLine();
+
+		deleteNodeThatContainsData(78);
+		printDLL();
+		ConsoleLogger.printLine();
+
+		insetNodeAtSpecificPsotion(new Node(1000), 2);
+		printDLL();
+		ConsoleLogger.printLine();
+
+		// delete all nodes
+		doublyLinkedList.headNode = null;
+
+		// Merge to LL
+		merge();
+		ConsoleLogger.printLine();
+
+	}
+
+	private static void merge() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void insetNodeAtSpecificPsotion(Node node, int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void deleteNodeThatContainsData(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void deleteNthNode(int i) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void getNodeByData(int key) {
+		// if head Node contains data
+		if (doublyLinkedList.headNode.data == key) {
+			System.out.println(key);
+		} else {
+
+			// Find the index of data
+			int index = 1;
+			
+			//TODO Suraj : review this logic
+			Node node = doublyLinkedList.headNode;
+
+			while (node != null) {
+				if (node.data == key) {
+					System.out.print(headNode.data);
+					System.out.println(index);
+					break;
+				}
+				node = node.nextNode;
+				index++;
+			}
+		}
+
+	}
+
+	private static void getNodeByIndex(int indexOfNode) {
+
+		// if index == 1 return head of the list
+		if (indexOfNode == 1) {
+			System.out.println(doublyLinkedList.headNode.data);
+		} else {
+			int index = 1;
+			// head node
+			Node headNode = doublyLinkedList.headNode;
+			// Until head is not null
+			while (headNode != null) {
+				// if index == indexOfNode
+				if (index == indexOfNode) {
+					System.out.println(headNode.data);
+					break;
+				}
+				headNode = headNode.nextNode;
+				index++;
+			}
+		}
+
 	}
 
 	private static void printWithAddresses() {
-		Node head = doublyLinkedList.head;
+		Node head = doublyLinkedList.headNode;
 		System.out.println("PN -- CN -- NN");
 		System.out.println("----------------");
 		while (head != null) {
@@ -56,7 +164,7 @@ public class DoublyLinkedList {
 
 	private static void printDLL() {
 
-		Node head = doublyLinkedList.head;
+		Node head = doublyLinkedList.headNode;
 		while (head != null) {
 			System.out.print(head.data + " ");
 			head = head.nextNode;
@@ -69,15 +177,15 @@ public class DoublyLinkedList {
 
 		Node newNode = new Node(i);
 
-		if (doublyLinkedList.head == null) {
-			doublyLinkedList.head = newNode;
+		if (doublyLinkedList.headNode == null) {
+			doublyLinkedList.headNode = newNode;
 		} else {
-			Node currentHead = doublyLinkedList.head;
-			doublyLinkedList.head = newNode;
+			Node currentHead = doublyLinkedList.headNode;
+			doublyLinkedList.headNode = newNode;
 			newNode.nextNode = currentHead;
 			currentHead.prevNode = newNode;
-			doublyLinkedList.head = newNode;
-			
+			doublyLinkedList.headNode = newNode;
+
 		}
 
 	}
